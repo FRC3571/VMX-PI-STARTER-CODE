51e8d42b217b04905428c648c57eb97782597f11
Eric Daviau <daviaue@hdsb.ca>
2022-06-07 10:59:09 -0400

    initial commit

---
+++ Java/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+
+public final class Constants
+{
+    /**
+     * Motor Constants
+     */
+    public static final int TITAN_ID        = 42;
+    public static final int MOTORR           = 0;
+    public static final int MOTORL           = 2;
+    public static final int SERVO_C         = 0;
+}
